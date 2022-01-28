11cd8cd2f1df6e32bd8c215db5d3cf45e97da12d
Hyeoni18 <gusgnl105@gmail.com>
2022-01-28 19:49:35 +0900

    [hyeoni] 수 정렬하기 풀이 - 정렬

---
+++ python_source/sorting/baekjoon/NumberSorting.py
@@ -0,0 +1,24 @@
+# <문제> 수 정렬하기
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+# <입력> 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+# <출력> 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+# <내 풀이>
+n = int(input())
+number = []
+for _ in range(n):
+    number.append(int(input()))
+
+number.sort()
+for i in range(n):
+    print(number[i])
+
+# <다른 사람 풀이 참고>
+import sys
+numbers=[]
+for i in range(int(input())):
+    numbers.append(int(sys.stdin.readline().rstrip()))
+
+for i in sorted(numbers):
+    print(i)
+
